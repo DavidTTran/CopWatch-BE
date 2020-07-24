7ff9951ef4d8b70f2ebbe3563d067913dfa0cfa5
Ben Fox <benfox1216@gmail.com>
2020-07-23 23:39:53 -0600

    Fix bug

--- config.py
+++ config.py
@@ -4,14 +4,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 from dotenv import load_dotenv
 load_dotenv()
 
-database = os.environ.get('DATABASE_URL')
-
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = os.environ['{database}']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
     DEBUG = False
