88593f48078749115269068334e126557840205d
David Tran <davidttemail@gmail.com>
2020-07-27 15:35:55 -0600

    Heroku errors

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ Manager(app)
 CORS(app)
 # load_dotenv()
 #
-# app.config.from_object(os.getenv['APP_SETTINGS'])
+app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import app, db
 
-# app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object(os.environ['APP_SETTINGS'])
 migrate = Migrate(app, db)
 manager = Manager(app)
 
